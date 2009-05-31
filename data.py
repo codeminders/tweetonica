77e26297b8f770da60fa13ac6d99fb3710b45116
Vadim Zaliva <lord@crocodile.org>
2009-05-31 13:57:12 -0700

    base model (without properties yet)

--- data.py
+++ data.py
@@ -3,16 +3,19 @@
 from google.appengine.ext import db
 
 
-class PUser(db.Model):
+class User(db.Model):
     twitter_username = db.StringProperty(required=True)
     twitter_password = db.StringProperty(required=True)
     last_updated = db.DateTimeProperty(auto_now_add=True)
 
-class Friend(db.Model):
-    pass
-
 class Group(db.Model):
-    pass
+    name = db.StringProperty(required=True)
+    puser = db.ReferenceProperty(User)
 
-class Timeline(db.Model):
-    pass
+class Friend(db.Model):
+    twitter_username = db.StringProperty(required=True)
+    group = db.ReferenceProperty(Group)
+
+class StatusUpdate(db.Model):
+    from_friend = db.ReferenceProperty(Friend)
+
