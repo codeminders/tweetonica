386c7eb25be635cf65acac38a4f9aa663def788c
Vadim Zaliva <lord@crocodile.org>
2009-06-14 14:39:13 -0700

    HTTP basic authentication

--- feed.py
+++ feed.py
@@ -2,8 +2,8 @@ import os
 
 import datetime
 import logging
-from urllib2 import HTTPError
 from uuid import uuid1
+from base64 import b64decode
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -12,11 +12,15 @@ from google.appengine.ext import db
 import twitter
 import data
 
+REALM='phanalgesfeed'
 
 class ATOMHandler(webapp.RequestHandler):
 
     def get(self):
-        pass
+        u = self._HTTP_authenticate()
+        if not u:
+            self.response.headers['WWW-Authenticate'] = 'Basic realm=%s' % REALM
+            self.response.set_status(401)
     
     def post(self):
         self.response.set_status(405)
@@ -44,6 +48,38 @@ class ATOMHandler(webapp.RequestHandler):
 
 
     # -- implementation method below  ---
+
+    def _HTTP_authenticate(self):
+        if not os.environ.has_key('HTTP_AUTHORIZATION'):
+            return None
+        ah = os.environ['HTTP_AUTHORIZATION']
+        logging.debug("Auth header: %s" % ah)
+        if not ah or len(ah)<6:
+            logging.warning("Invalid auth string '%s'" % ah)
+            return None
+        try:
+            ahd = b64decode(ah[6:])
+        except TypeError:
+            logging.warning("Error decoding auth string '%s'" % ah)
+            return None
+        ahds = ahd.split(':')
+        if len(ahds)!=2:
+            logging.warning("Error parsing auth string '%s'" % ahd)
+            return None
+        (username,password) = ahds
+        logging.debug("Authenticatin user '%s' with password '%s'" % \
+                      (username,password))
+
+        q = data.User.gql('WHERE screen_name = :1 and password=:2', \
+                          username,password)
+        users = q.fetch(1)
+        if len(users)==1:
+            logging.debug("User '%s' authenticated" % username)
+            return users[0]
+        else:
+            return None
+        
+        return None
 
     def _updateTimeLine(self,u,t):
         # TODO: since
