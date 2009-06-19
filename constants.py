25ced592805d7b9d566f312bd5b8a38a71e3cf35
Vadim Zaliva <lord@crocodile.org>
2009-06-18 17:38:56 -0700

    empty OPML handler

--- constants.py
+++ constants.py
@@ -4,3 +4,6 @@ DEFAULT_GROUP_NAME = "__ALL__"
 
 """ URL path of WSGI app for feed.py. Witout trailing slash! """
 FEED_PATH_PREFIX = "/feed"
+
+""" URL path of WSGI app for opml.py. Witout trailing slash! """
+OPML_PATH_PREFIX = "/opml"
