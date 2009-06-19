e6d7d1bb4e0cb5b201dae6061c52557b3e2138b5
Vadim Zaliva <lord@crocodile.org>
2009-06-18 17:42:58 -0700

    refactoring

--- opml.py
+++ opml.py
@@ -1,16 +1,13 @@
-import os
 
 import datetime
 import logging
 from uuid import uuid1
-from base64 import b64decode
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
 
 import queries
-import twitter
 import data
 import constants
 import misc
@@ -20,7 +17,7 @@ REALM='phanalgesfeed'
 class OPMLHandler(webapp.RequestHandler):
 
     def get(self, *args):
-        u = self._HTTP_authenticate()
+        u = misc.HTTP_authenticate()
         if not u:
             self.response.headers['WWW-Authenticate'] = 'Basic realm=%s' % REALM
             self.response.set_status(401)
@@ -53,39 +50,6 @@ class OPMLHandler(webapp.RequestHandler):
 
 
     # -- implementation method below  ---
-
-    def _HTTP_authenticate(self):
-        if not os.environ.has_key('HTTP_AUTHORIZATION'):
-            return None
-        ah = os.environ['HTTP_AUTHORIZATION']
-        logging.debug("Auth header: %s" % ah)
-        if not ah or len(ah)<6:
-            logging.warning("Invalid auth string '%s'" % ah)
-            return None
-        try:
-            ahd = b64decode(ah[6:])
-        except TypeError:
-            logging.warning("Error decoding auth string '%s'" % ah)
-            return None
-        ahds = ahd.split(':')
-        if len(ahds)!=2:
-            logging.warning("Error parsing auth string '%s'" % ahd)
-            return None
-        (username,password) = ahds
-        logging.debug("Authenticating user '%s' with password '%s'" % \
-                      (username,password))
-
-        q = data.User.gql('WHERE screen_name = :1 and password=:2', \
-                          username,password)
-        users = q.fetch(1)
-        if len(users)==1:
-            logging.debug("User '%s' authenticated" % username)
-            return users[0]
-        else:
-            logging.debug("No user of bad pass for %s" % username)
-            return None
-        
-        return None
 
     def _generateOPML(self, u):
         self.response.headers['Content-Type'] = 'application/opml+xml'
